94312cbc8232a148cf000ae934b1b6064e1e20f1
alissonrangel <alisson.rangel@hotmail.com>
2021-06-12 22:49:21 -0300

    Efeito de transição no menu lateral

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,15 +3,20 @@
 function menu_button() {
   let menu_lateral = document.getElementById('menu-lateral');
 
-  menu_lateral.style.display = 'block';  
+  menu_lateral.style.right = 0;
+  //menu_lateral.style.opacity = 1;
+  //menu_lateral.style.zIndex = 1000;
+  //menu_lateral.style.display = 'block';  
   // menu_lateral.classList.remove('display-none');
   // menu_lateral.classList.add('display-block');
 }
 
 function close_button() {
   let menu_lateral = document.getElementById('menu-lateral');
-
-  menu_lateral.style.display = 'none';
+  menu_lateral.style.right = '-220px';
+  //menu_lateral.style.opacity = 0;
+  //menu_lateral.style.zIndex = -1;
+  //menu_lateral.style.display = 'none';
   // menu_lateral.classList.remove('display-block');
   // menu_lateral.classList.add('display-none');
 }
